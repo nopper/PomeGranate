24ae3982510327e4c8a1c32f1c45a60ead10e3c7
Francesco Piccinno <stack.box@gmail.com>
2011-12-22 18:56:28 +0100

    We don't need replication

--- src/filesystem.py
+++ src/filesystem.py
@@ -2,7 +2,6 @@ from fsdfs.filesystem import Filesystem as DFSFilesystem
 
 class Filesystem(DFSFilesystem):
     def __init__(self, config):
-        config['replicatorIdleTime'] = 1
         DFSFilesystem.__init__(self, config)
 
     def getReplicationRules(self, filepath):
